5945bcae28bbe07560da98ee18a2c5a1ee11e6f5
Ryan C. Scott <rscott@roblox.com>
2023-06-25 14:57:53 -0400

    Reworked data mapping format
    Moved unit ID to end of name
    Removed multiple books mechanism (using unit names raw)

--- scripts/core.lua
+++ scripts/core.lua
@@ -1,74 +1,224 @@
 local _unitMapping = {}
 
-_unitMapping["Alien Hives"] = {
+_unitMapping = {
    ["Hive Lord"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936128171/F0C0FC65F81A2AFDEC91CA2F92421FC1E8A3CA70/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936167479/A2B103E6B11ECA6E4E1439E185BE72197EF2596A/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936128171/F0C0FC65F81A2AFDEC91CA2F92421FC1E8A3CA70/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936167479/A2B103E6B11ECA6E4E1439E185BE72197EF2596A/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+      },
+   },
+
+   ["Snatcher Lord"] = {
+      {
+         scale = 0.75,
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/2f9b89c8d13d00af0de475c97b2cbdea_large.png?v=1670973865",
+      },
    },
 
    ["Prime Warrior"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936130483/5D77BF55DFE7DEDEC8C01369D1D6536805431F34/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936154303/106ACFA61BBF93A16FFE5ECAEEB618A9AEFB4D60/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
-   },
-
-   ["Hive Swarm"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936178055/5D9FA2057E4AAA73EE580279BB4309E72B9055A4/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936178227/B9337A11A1C82458BF1CF6539F20833480ABF87F/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
-   },
-
-   ["Winged Grunts"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936133373/DCA1475D91F3F0D74ACD66F9B4C7F3F0256AEF9D/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936164777/AB76388A4C08353DB7D7F842F956E445B45AC725/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
-   },
-   
-   ["Hive Warriors"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936118486/6FF029D2AD24A2361C49B09CE5EEAC7B4AB7DD68/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936172187/3ADA7DFBD90E06CB211FEB828CAFAE234435A391/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
-   },
-
-   ["Carnivo-Rex"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/e97537cd1ee08475d9d10ab333e1fa1b_large.png?v=1670973865",
+      }
    },
 
    ["Assault Grunts"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936144008/430E198932D29C4F903B81B9C9DB752FF10E83EC/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936168997/2867D4E21E85873BCB6B097B2CB7A5DF23669213/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936144008/430E198932D29C4F903B81B9C9DB752FF10E83EC/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936168997/2867D4E21E85873BCB6B097B2CB7A5DF23669213/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      },
    },
 
-   ["Spores"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936135718/4424E3EAB0BEECA054DD3CF0686688912F4EDE11/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936157066/40AA0E956876077F440D5004C28DB0847E7B8E74/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+   ["Shooter Grunts"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936130483/5D77BF55DFE7DEDEC8C01369D1D6536805431F34/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936154303/106ACFA61BBF93A16FFE5ECAEEB618A9AEFB4D60/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      },
+   },
+   
+   ["Winged Grunts"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936133373/DCA1475D91F3F0D74ACD66F9B4C7F3F0256AEF9D/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936164777/AB76388A4C08353DB7D7F842F956E445B45AC725/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      },
    },
 
-   ["Invasion Artillery Spore"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936128756/9D94FC33977DDA058776D1B62038197DCFF03828/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936167096/BF68FB70471EA8085C9B7E357A24B0E2893E48C2/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+   ["Soul-Snatchers"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936145595/0EE2ABB7760DCBE1B2196DDE94983A1BA3538766/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936179018/3DB22B484906E5F91ADE4F48A3BCE97A60A1CA28/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      },
    },
 
-   ["Invasion Carrier Spore"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936141793/B831F82C5D20F70C475C1B6660595433D62CF46F/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936171405/FC3D654B4FA52CCCED29F1081E33EDF86A421318/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936139764/3A10398982B240DBE6215E83F172C863EDC1938C/",
+   ["Hive Warriors"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936118486/6FF029D2AD24A2361C49B09CE5EEAC7B4AB7DD68/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936172187/3ADA7DFBD90E06CB211FEB828CAFAE234435A391/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      },
+   },
+
+   ["Hive Guardians"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936143619/A2BF7B4252960C1231132DFFE709D702D852CEAB/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936169523/6A2AC8C7C968BFF0B0DB90B6EF8584C1DAA1BA49/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      },
+   },
+
+   ["Hive Swarm"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936178055/5D9FA2057E4AAA73EE580279BB4309E72B9055A4/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936178227/B9337A11A1C82458BF1CF6539F20833480ABF87F/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      },
+   },
+
+   ["Ravenous Beasts"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/5b2172b733cb6fbdc677692fab2398a6_large.png?v=1670973865",
+      }
+   },
+
+   ["Synapse Floaters"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936135162/38B5548773B28B2034956ED29396A885104A369B/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936166902/A896D2CC400DEDF14A5A858FDAC806F9194D8233/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      },
    },
 
    ["Venom Floaters"] = {
-      mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936135162/38B5548773B28B2034956ED29396A885104A369B/",
-      diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936166902/A896D2CC400DEDF14A5A858FDAC806F9194D8233/",
-      collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936135162/38B5548773B28B2034956ED29396A885104A369B/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936166902/A896D2CC400DEDF14A5A858FDAC806F9194D8233/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936109828/2C84DE4EB0D1401E132818075B29FD130E2A1F4F/",
+      },
+   },
+
+   ["Shadow Hunter"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/215fe8fbc73ef0ac36030525ecdcf556_large.png?v=1646176995",
+      },
+   },
+
+   ["Carnivo-Rex"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/1705161081448427031/81C2712F7382E611B13DC6C4B3A12924B7211515/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936162764/E89895BB42911D7156207FE08CBC035618CDBEF8/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+      },
+   },
+
+   ["Toxico-Rex"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936131510/E7D9B23FEDB551AEA6120875BE5551F94F5D22FB/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936156320/983CA4773DAFF4661E54E6BBB21558EC12D0BFEA/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936129447/1700E6D93E93DE440EDC437B09DB027C863296D4/",
+      },
+   },
+
+   ["Psycho-Rex"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936131510/E7D9B23FEDB551AEA6120875BE5551F94F5D22FB/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936156320/983CA4773DAFF4661E54E6BBB21558EC12D0BFEA/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936129447/1700E6D93E93DE440EDC437B09DB027C863296D4/",
+      },
+   },
+
+   -- ["Devourer Beast"] = {
+   --    {
+   --       face = "",
+   --    }
+   -- },
+
+   -- ["Tyrant Beast"] = {
+   --    {
+   --       face = "",
+   --    }
+   -- },
+
+   -- ["Spawning Beast"] = {
+   --    {
+   --       face = "",
+   --    }
+   -- },
+
+   -- ["Artillery Beast"] = {
+   --    {
+   --       face = "",
+   --    }
+   -- },
+
+   ["Burrower"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/82fe16813b9a865b18f4a14b91fc5164_large.png?v=1670973870",
+      }
+   },
+
+   ["Flamer Beast"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/bd530bec408200ce86c314330b3ace81_large.png?v=1670973870",
+      }
+   },
+
+   ["Mortar Beast"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/c6f5da6dfbbb8aa11fb6ec53f55882eb_large.png?v=1670973870",
+      },
+   },
+
+   ["Invasion Carrier Spore"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936141793/B831F82C5D20F70C475C1B6660595433D62CF46F/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936171405/FC3D654B4FA52CCCED29F1081E33EDF86A421318/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936139764/3A10398982B240DBE6215E83F172C863EDC1938C/",
+      },
+   },
+   
+   ["Invasion Artillery Spore"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936128756/9D94FC33977DDA058776D1B62038197DCFF03828/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936167096/BF68FB70471EA8085C9B7E357A24B0E2893E48C2/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936102283/3E6999C8480E54A2E682797D4A7303765EFE03B3/",
+      },
+   },
+   
+   -- ["Repacious Beast"] = {
+   --    {
+   --       face = "",
+   --    }
+   -- },
+
+   ["Hive Titan"] = {
+      {
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/56b750a7fca790becb11b844c4e31807_large.png?v=1670973871",
+      }
+   },
+
+   ["Spores"] = {
+      {
+         mesh = "http://cloud-3.steamusercontent.com/ugc/786374678936135718/4424E3EAB0BEECA054DD3CF0686688912F4EDE11/",
+         diffuse = "http://cloud-3.steamusercontent.com/ugc/786374678936157066/40AA0E956876077F440D5004C28DB0847E7B8E74/",
+         collider = "http://cloud-3.steamusercontent.com/ugc/786374678936133623/E1B8842A5BB217612EE3E386C71A65032236C2D0/",
+      },
+   },
+
+   ["Massive Spore"] = {
+      {
+         scale = 1.0,
+         face = "https://cdn.shopify.com/s/files/1/0299/7711/6804/products/2b5b2c39b4cfde443745f33eb7365569_large.png?v=1670973870",
+      }
    },
 
    _default = {
-      assetbundle = "http://cloud-3.steamusercontent.com/ugc/946222093771739578/CBC341707C193AA6E6F1EAACB700A615889EC227/"
+      {
+         assetbundle = "http://cloud-3.steamusercontent.com/ugc/946222093771739578/CBC341707C193AA6E6F1EAACB700A615889EC227/"
+      }
    },
 }
 
@@ -134,18 +284,8 @@ function copyPos(pos)
    }
 end
 
-function matchUnit(mapping, books, unitData)
-   local matches = {}
-
-   for _, book in ipairs(books) do
-      local map = mapping[book]
-
-      if map then
-         local model = map[unitData.name] or map._default
-         table.insert(matches, model)
-      end
-   end
-
+function matchUnit(mapping, unitData)
+   local matches = mapping[unitData.name] or mapping._default
    return matches
 end
 
@@ -196,7 +336,7 @@ function processUnitLoadout(unit, out)
    end
 end
 
-function spawnUnit(books, data, pos)
+function spawnUnit(data, pos)
    local maxX = pos.x
    local cursor = copyPos(pos)
    local maxSize = Vector()
@@ -204,20 +344,11 @@ function spawnUnit(books, data, pos)
    local modelCount = data.size * (data.combined and 2 or 1)
 
    for i = 1, modelCount do
-      local scale = 0.5
-      local modelVariants = matchUnit(_unitMapping, books, data)
+      local modelVariants = matchUnit(_unitMapping, data)
       local model = modelVariants[1]
+      local scale = model.scale or 0.5
 
       -- TODO: Refactor so all variants can be context menu items
-
-      local modelType
-      if model.assetbundle then
-         modelType = "Custom_Assetbundle"
-
-      elseif model.mesh then
-         modelType = "Custom_Model"
-      end
-
       local col = math.floor((i - 1) % _modelsPerRow)
       local row = math.floor((i - 1) / _modelsPerRow)
 
@@ -228,23 +359,53 @@ function spawnUnit(books, data, pos)
 
       maxX = math.max(spawnPos.x, maxX)
 
-      local obj = spawnObject({
-            type = modelType,
-            position = spawnPos,
-            scale = Vector(scale, scale, scale),
-      })
+      local modelType
+      local objData = {
+         position = spawnPos,
+         scale = Vector(scale, scale, scale),
+      }
 
-      obj.setCustomObject(T.merge(model, {
-              type = 1,
-              material = 3,
-      }))
+      if model.assetbundle then
+         objData = T.merge(objData, {
+                 type = "Custom_Assetbundle",
+         })
+
+         model = T.merge(model, {
+                 type = 1,
+                 material = 3,
+         })
+
+      elseif model.mesh then
+         objData = T.merge(objData, {
+                 type = "Custom_Model",
+         })
+
+         model = T.merge(model, {
+                 type = 1,
+                 material = 3,
+         })
+
+      elseif model.face then
+         objData = T.merge(objData, {
+                 type = "CardCustom",
+                 rotation = Vector(0, 180, 0),
+         })
+
+         model = T.merge(model, {
+                 type = 4,
+                 back = "https://uploads-ssl.webflow.com/63615fdab9c39472c7fcfe4f/6361616586af540d18a28344_onepagerules_round_2.png",
+         })
+      end
+
+      local obj = spawnObject(objData)
+      obj.setCustomObject(model)
 
       obj.setName(string.format(
-              "%s -- %s [b][00eeee]Q%s[-] [Ffc125]D%s[-][/b]",
-              data.id,
+              "%s [b][00eeee]Q%s[-] [Ffc125]D%s[-][/b] \[%s\]",
               data.name,
               data.quality,
-              data.defense))
+              data.defense,
+              data.id))
 
       local grey = 16 / 255
       obj.setColorTint(Color(grey, grey, grey))
@@ -276,8 +437,7 @@ function spawnList(data, pos)
 
    for _, unit in ipairs(data.units) do
       if not seen[unit.id] then
-         -- TODO: Handle multiple books
-         local obj, pos = spawnUnit({data.name}, unit, pos)
+         local obj, pos = spawnUnit(unit, pos)
          seen[unit.id] = true
       end
    end
